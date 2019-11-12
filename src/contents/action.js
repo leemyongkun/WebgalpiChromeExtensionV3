5a5111189fe3c48122a5b85c58e09c3c3c14dad3
임용근 <ykleem@coinone.com>
2019-11-12 17:49:46 +0900

    update

--- src/contents/action.js
+++ src/contents/action.js
@@ -205,8 +205,8 @@ let CONTENT_ACTION = {
 
     // 드래그 후 바로 '메모'입력 버튼을 눌렀을 경우에는 사라지지 않도록 한다.
     /* if (memoFlag === undefined) {
-              $('#highlight-toolbar').hide();
-            } */
+                  $('#highlight-toolbar').hide();
+                } */
 
     CORE.executeHighlight(param); //화면에 하이라이팅 하기
     FORM.clearColorPicker(param.COLOR); //color picker 버튼 초기화
@@ -426,14 +426,14 @@ let EVENT = {
           event.target.nodeName === GLOBAL_CONFIG.HL_TAG_NAME.toUpperCase()
         ) {
           /* let currentId;
-                              STATUS.mouseUpId = $(event.target).attr(HighlightData.idName);
-                              if (HighlightCore.isNumber(STATUS.mouseUpId)) {
-                                currentId = STATUS.mouseUpId;
-                              } */
+                                        STATUS.mouseUpId = $(event.target).attr(HighlightData.idName);
+                                        if (HighlightCore.isNumber(STATUS.mouseUpId)) {
+                                          currentId = STATUS.mouseUpId;
+                                        } */
 
           /* if (HighlightCore.isNumber(HighlightData.downId)) {
-                                currentId = HighlightData.downId;
-                              } */
+                                          currentId = HighlightData.downId;
+                                        } */
           STATUS.mouseDownFlag = false;
           return false;
         }
@@ -450,11 +450,11 @@ let EVENT = {
         // 위젯영역일경우 컬러 팔레트를 보여주지 않는다.
         // 컬러 피커가 사용하지 않음일경우 보여주지 않는다.
         /* chrome.storage.sync.get(['options'], result => {
-                           let highlightYN = result.options.HIGHLIGHT;
-                           if (STATUS.widgetArea === 0 && highlightYN === 'Y') {
-                             HighlightCore.mouseDragAction(event); // todo 가장 중요!!
-                           }
-                         }); */
+                                   let highlightYN = result.options.HIGHLIGHT;
+                                   if (STATUS.widgetArea === 0 && highlightYN === 'Y') {
+                                     HighlightCore.mouseDragAction(event); // todo 가장 중요!!
+                                   }
+                                 }); */
 
         CONTENT_ACTION.setHighlightRangeInfoData(event, offset);
         FORM.showPicker(event); // todo 가장 중요!!
@@ -475,9 +475,9 @@ let EVENT = {
 
             // 로그인 되어있지 않다면 위젯을 열어준다.
             /* if (!HighlightData.isLogin) {
-                                        HlLayerAction.loginCheckLayer();
-                                        return false;
-                                    } */
+                                                    HlLayerAction.loginCheckLayer();
+                                                    return false;
+                                                } */
 
             let _this = this;
             // 이미 선택되어진 컬러의 경우..
@@ -510,11 +510,14 @@ let EVENT = {
               .find(".wafflepen-color-picker a")
               .each((idx, item) => {
                 if ($(item).hasClass("on")) {
+                  console.log("on!!");
                   colorTF = true;
                 }
               })
               .promise()
               .then(function() {
+                console.log("colorTF ", colorTF);
+                return false;
                 // 저장한다.
                 if (colorTF) {
                   // 클릭할 경우 수정
