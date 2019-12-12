6a770aa9eb0c04d94d6d62a59590024274b6de5b
임용근 <ykleem@careercare.co.kr>
2019-12-13 00:37:32 +0900

    update

--- src/popup/App.vue
+++ src/popup/App.vue
@@ -18,16 +18,29 @@
       ></el-button>
     </el-header>
 
-    <el-main>
+    <el-main style="padding: 3px;">
       <el-tabs tab-position="left" style="height: 320px;">
         <el-tab-pane label="INFO">
           <div style="width: 380px">
-            <span class="demonstration">Custom</span>
-            <el-image :src="src" fit="fill">
-              <div slot="placeholder" class="image-slot">
-                Loading<span class="dot">...</span>
-              </div>
-            </el-image>
+            <div>
+              <span style="height: 10px;">Custom</span>
+            </div>
+            <div>
+              <el-image :src="src" fit="fill">
+                <div slot="placeholder" class="image-slot">
+                  Loading<span class="dot">...</span>
+                </div>
+              </el-image>
+            </div>
+            <div>
+              <span>
+                ---- 소스 네비게이션 ----- Ctrl + 마우스커서(혹은 F3) : 클래스나
+                메소드 혹은 멤버를 상세하게 검색하고자 할때 Alt + Left, Alt +
+                Right : 이후, 이전 Ctrl + O : 해당 소스의 메소드 리스트를
+                확인하려 할때 F4 : 클래스명을 선택하고 누르면 해당 클래스의
+                Hierarchy 를
+              </span>
+            </div>
           </div>
         </el-tab-pane>
 
@@ -52,50 +65,49 @@
             </div>
           </el-scrollbar>
         </el-tab-pane>
-        <el-tab-pane label="Role">Role</el-tab-pane>
-        <el-tab-pane label="Task">Task</el-tab-pane>
+        <el-tab-pane label="OPTION">OPTION</el-tab-pane>
       </el-tabs>
     </el-main>
   </el-container>
 
   <!-- el-tabs tab-position="left" style="height: 320px;">
-        <el-tab-pane label="INFO">
+          <el-tab-pane label="INFO">
 
-            <div style="width: 380px">
-                <span class="demonstration">Custom</span>
-                <el-image :src="src" fit="fill">
-                    <div slot="placeholder" class="image-slot">
-                        Loading<span class="dot">...</span>
-                    </div>
-                </el-image>
-            </div>
-        </el-tab-pane>
+              <div style="width: 380px">
+                  <span class="demonstration">Custom</span>
+                  <el-image :src="src" fit="fill">
+                      <div slot="placeholder" class="image-slot">
+                          Loading<span class="dot">...</span>
+                      </div>
+                  </el-image>
+              </div>
+          </el-tab-pane>
 
 
-        <el-tab-pane label="HIGHLIGT">
-            <el-scrollbar wrap-class="list" :native="false">
-                <div style="height: 300px;">
-                    <el-timeline
-                            style="padding-left: 2px; margin-top: 12px; margin-right: 20px;"
-                    >
-                        <el-timeline-item
-                                v-for="(activity, index) in Highlight.activities"
-                                :key="index"
-                                :icon="activity.icon"
-                                :type="activity.type"
-                                :color="activity.color"
-                                :size="activity.size"
-                                :timestamp="activity.timestamp"
-                        >
-                            {{ activity.content }}
-                        </el-timeline-item>
-                    </el-timeline>
-                </div>
-            </el-scrollbar>
-        </el-tab-pane>
-        <el-tab-pane label="Role">Role</el-tab-pane>
-        <el-tab-pane label="Task">Task</el-tab-pane>
-    </el-tabs -->
+          <el-tab-pane label="HIGHLIGT">
+              <el-scrollbar wrap-class="list" :native="false">
+                  <div style="height: 300px;">
+                      <el-timeline
+                              style="padding-left: 2px; margin-top: 12px; margin-right: 20px;"
+                      >
+                          <el-timeline-item
+                                  v-for="(activity, index) in Highlight.activities"
+                                  :key="index"
+                                  :icon="activity.icon"
+                                  :type="activity.type"
+                                  :color="activity.color"
+                                  :size="activity.size"
+                                  :timestamp="activity.timestamp"
+                          >
+                              {{ activity.content }}
+                          </el-timeline-item>
+                      </el-timeline>
+                  </div>
+              </el-scrollbar>
+          </el-tab-pane>
+          <el-tab-pane label="Role">Role</el-tab-pane>
+          <el-tab-pane label="Task">Task</el-tab-pane>
+      </el-tabs -->
 </template>
 
 <script>
