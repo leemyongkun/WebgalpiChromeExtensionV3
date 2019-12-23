aa67a2f7588503fb7f11bc64b9d3051b28a1aaa3
임용근 <ykleem@careercare.co.kr>
2019-12-23 19:22:18 +0900

    update

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -2,6 +2,9 @@ import Vue from "vue";
 import PopupApp from "./PopupApp";
 import BootstrapVue from "bootstrap-vue";
 
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
 //global.browser = require("webextension-polyfill");
 Vue.use(BootstrapVue);
 
