dc4a5e4c384555bbf43ac7cca2cbe0bbc893642e
ykleem <>
2019-12-23 13:28:47 +0900

    element-ui 흔적 지우기

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,12 +1,8 @@
 import Vue from "vue";
 import App from "./App";
-import ElementUI from "element-ui";
-import "element-ui/lib/theme-chalk/index.css";
-import locale from "element-ui/lib/locale/lang/ko";
 
 global.browser = require("webextension-polyfill");
 
-Vue.use(ElementUI, { locale });
 /* eslint-disable no-new */
 new Vue({
   el: "#app",
