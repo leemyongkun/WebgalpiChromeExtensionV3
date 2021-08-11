7ad81fa39e440ac3d4aabe2c3349f34d8f4fb9ff
임용근 <kkuni.bear@gmail.com>
2021-08-11 23:40:23 +0900

    update

--- src/background/background.js
+++ src/background/background.js
@@ -1,7 +1,5 @@
-import store from "../store";
-
 global.browser = require("webextension-polyfill");
-let md5 = require("md5");
+
 import dbcon from "../database/dbcon.js";
 import Api from "../api/api.js";
 import LANG from "../common/language";
@@ -24,56 +22,43 @@ let BackgroundModule = {
       return false;
     }
 
-    //차단된 사이트는 열지 않도록 한다.
-    let isDetected = false;
-    for (var i = 0; i < SITE_MANAGER.DETECTE_SITES.length; i++) {
-      if (currentUrl.indexOf(SITE_MANAGER.DETECTE_SITES[i]) !== -1) {
-        isDetected = true;
-        chrome.browserAction.setPopup({
-          tabId: tabId,
-          popup: "popup/detectPopup.html"
-        });
+    /** 차단된 사이트는 열지 않도록 한다. */
+    if (BackgrounEvent.checkExcludeSite(tabId, currentUrl)) return false;
 
-        chrome.browserAction.setBadgeText({
-          text: "X",
-          tabId: tabId
-        });
-        chrome.browserAction.setBadgeBackgroundColor({
-          color: "red",
-          tabId: tabId
-        });
-
-        return false;
-      }
-    }
-    if (isDetected) {
-      return false;
-    }
-
-    let urlPath = currentUrl;
-    let ext = urlPath.substr(urlPath.length - 4, urlPath.length);
-    currentUrl = md5(currentUrl.split("#")[0]);
-
-    let initParameter = {
-      URL_KEY: currentUrl,
-      URL: urlPath,
-      EXT: ext
-    };
-
-    //현재 urlKey를 저장한다.
+    /** 현재 urlKey를 저장한다. */
     chrome.storage.local.set({ [tabId]: currentUrl }, null);
-    4;
-    //EMAIL로 조건을 걸지 않고, 사용중(IS_USE=Y)의 데이타만 가져온다
+
+    //todo : N번 호출 됨.
+
+    /** EMAIL로 조건을 걸지 않고, 사용중(IS_USE=Y)의 데이타만 가져온다 */
     Api.getMemberInfo().then(memberInfo => {
       if (memberInfo.EMAIL === "") {
         return false;
       }
-      //로그인 정보 저장해둔다.
+      /**  로그인 정보 저장해둔다.*/
       chrome.storage.local.set({ loginInfo: memberInfo });
+
+      let urlPath = currentUrl;
+      let ext = urlPath.substr(urlPath.length - 4, urlPath.length);
+      currentUrl = Common.generatorUrlKey(currentUrl, memberInfo.EMAIL);
+
+      let initParameter = {
+        URL_KEY: currentUrl,
+        URL: urlPath,
+        EXT: ext
+      };
 
       initParameter.EMAIL = memberInfo.EMAIL;
 
       Api.getInitInfo(initParameter).then(res => {
+        if (res.isRegist) {
+          let highlightSize =
+            res.allItems.HIGHLIGHT_LIST == null
+              ? 0
+              : res.allItems.HIGHLIGHT_LIST.length;
+          BackgrounEvent.setBadge(tabId, a.toString(), "green");
+        }
+
         //todo : excludesUrl 등록 기능 추가 할것.
         res.tabid = tabId;
 
@@ -89,7 +74,34 @@ let BackgroundModule = {
     });
   }
 };
+
 let BackgrounEvent = {
+  setBadge: (tabId, _text, _color) => {
+    chrome.browserAction.setBadgeText({
+      text: _text,
+      tabId: tabId
+    });
+    chrome.browserAction.setBadgeBackgroundColor({
+      color: _color,
+      tabId: tabId
+    });
+  },
+  checkExcludeSite: (tabId, currentUrl) => {
+    let isDetected = false;
+    for (var i = 0; i < SITE_MANAGER.DETECTE_SITES.length; i++) {
+      if (currentUrl.indexOf(SITE_MANAGER.DETECTE_SITES[i]) !== -1) {
+        isDetected = true;
+        chrome.browserAction.setPopup({
+          tabId: tabId,
+          popup: "popup/detectPopup.html"
+        });
+        BackgrounEvent.setBadge(tabId, "X", "red");
+        return false;
+      }
+    }
+
+    return isDetected;
+  },
   onInstalled: () => {
     chrome.runtime.onInstalled.addListener(details => {
       if (details.reason === "install") {
@@ -113,9 +125,7 @@ let BackgrounEvent = {
 
         //테이블 추가
         dbcon.addTable();
-
         dbcon.removeTable();
-
         //업데이트 Alert 출력
 
         chrome.notifications.create("", opt);
@@ -152,22 +162,21 @@ let BackgrounEvent = {
   },
   onUpdated: () => {
     chrome.tabs.onUpdated.addListener((tabId, info, tab) => {
-      //console.log("complte에서 loading으로 변경함.. 나중에 수정해야할 수도.. ",info.status);
-      //if (info.status === "complete") {
-      //loading
-      //팝업인지 확인.
-      BackgroundModule.isPopup();
+      //todo : console.log("complte에서 loading으로 변경함.. 나중에 수정해야할 수도.. ",info.status);
+      if (info.status === "complete") {
+        //loading
+        /** 팝업인지 확인.*/
+        BackgroundModule.isPopup();
 
-      //대쉬보드의 TABID를 저장해둔다. (재진입시 STATUS를 변경하여 리로딩 하기 위함)
-      if (tab.url === Common.getDashboardUrl()) {
-        chrome.storage.local.set({
-          activeDashboardTabId: tab.id
-        });
+        /** 대쉬보드의 TABID를 저장해둔다. (재진입시 STATUS를 변경하여 리로딩 하기 위함) */
+        if (tab.url === Common.getDashboardUrl()) {
+          chrome.storage.local.set({
+            activeDashboardTabId: tab.id
+          });
+        }
+        /** 현재 사이트에 하이라이트 초기화 */
+        BackgroundModule.initApplication(tabId, tab.url);
       }
-
-      //현재 사이트에 하이라이트 초기화
-      BackgroundModule.initApplication(tabId, tab.url);
-      //}
     });
   },
   autoBackupScheduler: () => {
@@ -194,18 +203,18 @@ function setBadgeColor(color) {
   });
 }
 
-//설치 및 리로딩시
+/** 설치 및 리로딩시 */
 BackgrounEvent.onInstalled();
 
-//Tab이 열릴때
+/** Tab이 열릴때 */
 BackgrounEvent.onUpdated();
 
-BackgrounEvent.autoBackupScheduler();
+//BackgrounEvent.autoBackupScheduler();
 
 //Bookmark
 //BackgrounEvent.getBookmarks();
 
-//DashBoard에 재진입 했을때 데이타를 다시 가져온다.
+/** DashBoard에 재진입 했을때 데이타를 다시 가져온다. */
 chrome.tabs.onActivated.addListener((activeInfo, act) => {
   chrome.storage.local.get(["activeDashboardTabId"], result => {
     if (result.activeDashboardTabId === activeInfo.tabId) {
@@ -214,28 +223,17 @@ chrome.tabs.onActivated.addListener((activeInfo, act) => {
   });
 });
 
-export default BackgroundModule;
-/*
-chrome.webRequest.onHeadersReceived.addListener(
-  function(info) {
-    var headers = info.responseHeaders;
-    for (var i = headers.length - 1; i >= 0; --i) {
-      var header = headers[i].name.toLowerCase();
-      if (header == "x-frame-options" || header == "frame-options") {
-        headers.splice(i, 1); // Remove header
-      }
+/** Window에 Focus Event 발생 시*/
+/*chrome.windows.onFocusChanged.addListener(function(window) {
+    if (window != chrome.windows.WINDOW_ID_NONE) {
+        console.log("onFocus");
+        SYNC.SERVER();
     }
-    return { responseHeaders: headers };
-  },
-  {
-    urls: ["*://!*!/!*"], // Pattern to match all http(s) pages
-    types: ["sub_frame"]
-  },
-  ["blocking", "responseHeaders"]
-);
+});*/
+/** 새탭이 열릴때 서버에서 싱크 */
+/*chrome.tabs.onCreated.addListener(callback => {
+    console.log("onCreate");
+    SYNC.SERVER();
+});*/
 
-permission
-    "webRequest",
-    "webRequestBlocking"
-
-*/
+export default BackgroundModule;
