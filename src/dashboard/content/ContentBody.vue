77aabf8728c4e430a7f03ef7d9ebc406671ade03
leemyongkun <kkuni.bear@gmail.com>
2022-02-11 01:52:25 +0900

    update size & color

--- src/dashboard/content/ContentBody.vue
+++ src/dashboard/content/ContentBody.vue
@@ -1,149 +1,174 @@
 <template xmlns:v-slot="http://www.w3.org/1999/XSL/Transform">
   <div>
     <v-row>
-      <v-col cols="3" class="overflow-y-auto"
+      <v-col cols="3" class="overflow-y-auto pl-2 pr-2"
         ><!--:style="documentHeightStyle"-->
-        <v-row>
-          <v-col class="pb-0 pt-0 mb-2">
-            <!-- Option -->
-            <FilterDialog :itemCount="itemCount"></FilterDialog>
-          </v-col>
-        </v-row>
-        <v-row
-          :style="listAreaHeightStyle"
-          style="overflow-x:hidden;"
-          class="overflow-y-auto custom-scroll"
-        >
-          <v-col v-if="sites.length === 0">
-            <v-card class="mx-auto">
-              <v-card-text>
-                <p class="display-1 text--primary">
-                  NO CONTENTS
-                </p>
-                <div class="text--primary">
-                  {{ LANG.DESCRIPTION_MESSAGE("D0021") }}<br />
-                </div>
-              </v-card-text>
-              <v-card-actions>
-                <v-btn text outlined color="green accent-4" @click="learnSite">
-                  {{ LANG.BUTTON_MESSAGE("B0006") }}
-                </v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-col>
 
-          <v-col v-else class="pt-0">
-            <!-- 컨텐츠 리스트 -->
-            <v-list style="background: none;padding: 0;" id="contentsList">
-              <div v-for="(item, index) in sites" :key="index">
-                <v-row>
-                  <v-col cols="12" style="padding-top: 0px;">
-                    <drag :transfer-data="item" :class="item.CLASS">
-                      <!-- DRAG 시 보이는 영역-->
-                      <div slot="image" class="drag-image">
-                        <v-chip class="ma-2" color="orange" text-color="white">
-                          {{ item.UPDATE_TITLE }}
-                          <v-icon right>mdi-star</v-icon>
-                        </v-chip>
-                      </div>
-                      <!------------------>
-                      <v-hover v-slot:default="{ hover }">
-                        <v-card
-                          aria-selected="true"
-                          class="mx-auto"
-                          outlined
-                          style="cursor:pointer;"
-                          @click="selectSite(item, item.URL_KEY)"
-                          @dblclick="goSourceSite(item, $event)"
-                          :key="item.URL_KEY"
-                          ref="siteList"
-                        >
-                          <v-list-item three-line>
-                            <v-expand-transition>
-                              <div
-                                v-if="hover"
-                                class="v-card--reveal white--text text-lg-right pr-3"
-                                style="height: 30%;z-index: 99;"
-                              >
-                                <v-btn
-                                  @click="setFavorite($event, item)"
-                                  small
-                                  color="warning"
-                                >
-                                  <v-icon>mdi-star</v-icon>
-                                </v-btn>
-                              </div>
-                            </v-expand-transition>
+        <v-card flat :style="listPanelAreaHeightStyle">
+          <v-card-text>
+            <v-row>
+              <v-col class="pb-0 pt-0 mb-2">
+                <!-- Option -->
+                <FilterDialog :itemCount="itemCount"></FilterDialog>
+              </v-col>
+            </v-row>
+            <v-row
+              :style="listAreaHeightStyle"
+              style="overflow-x:hidden;"
+              class="overflow-y-auto custom-scroll"
+            >
+              <v-col v-if="sites.length === 0">
+                <v-card class="mx-auto">
+                  <v-card-text>
+                    <p class="display-1 text--primary">
+                      NO CONTENTS
+                    </p>
+                    <div class="text--primary">
+                      {{ LANG.DESCRIPTION_MESSAGE("D0021") }}<br />
+                    </div>
+                  </v-card-text>
+                  <v-card-actions>
+                    <v-btn
+                      text
+                      outlined
+                      color="green accent-4"
+                      @click="learnSite"
+                    >
+                      {{ LANG.BUTTON_MESSAGE("B0006") }}
+                    </v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-col>
 
-                            <v-list-item-content>
-                              <v-list-item-title style="padding-bottom: 5px;">
-                                {{ item.UPDATE_TITLE }}
-                              </v-list-item-title>
-                              <v-list-item-subtitle style="font-size: 13px;">
-                                <span
-                                  v-if="item.OG_DESCRIPTION !== 'undefined'"
-                                  >{{ item.OG_DESCRIPTION }}</span
-                                >
-                              </v-list-item-subtitle>
-                              <v-list-item-subtitle
-                                style="font-size: 10px; margin-top: 4px;"
-                              >
-                                <v-icon
-                                  size="16px"
-                                  color="green"
-                                  left
-                                  style="margin-right: 1px;"
-                                  v-if="item.CATEGORY_NAME !== 'NO_CATEGORY'"
-                                  >mdi-folder-outline
-                                </v-icon>
-                                {{ item.CATEGORY_NAME }}
-
-                                <v-icon
-                                  size="16px"
-                                  color="yellow"
-                                  left
-                                  style="margin-right: 1px;"
-                                  v-if="item.FL_FAVORITE === 'Y'"
-                                  >mdi-star
-                                </v-icon>
-                                <v-icon
-                                  size="16px"
-                                  color="red"
-                                  left
-                                  style="margin-right: 1px;"
-                                  v-if="item.FL_READMODE === 'N'"
-                                  >mdi-shield-off-outline
-                                </v-icon>
-                              </v-list-item-subtitle>
-                            </v-list-item-content>
-
-                            <v-list-item-avatar
-                              tile
-                              size="70"
-                              style="margin-top: 14px;padding-bottom: 0px;margin-bottom: 0px;"
-                              color="grey"
+              <v-col v-else class="pt-0">
+                <!-- 컨텐츠 리스트 -->
+                <v-list style="background: none;padding: 0;" id="contentsList">
+                  <div v-for="(item, index) in sites" :key="index">
+                    <v-row>
+                      <v-col cols="12" style="padding-top: 0px;">
+                        <drag :transfer-data="item" :class="item.CLASS">
+                          <!-- DRAG 시 보이는 영역-->
+                          <div slot="image" class="drag-image">
+                            <v-chip
+                              class="ma-2"
+                              color="orange"
+                              text-color="white"
                             >
-                              <v-img
-                                v-if="item.OG_IMAGE !== 'undefined'"
-                                :src="checkImagePath(item.OG_IMAGE)"
-                              ></v-img>
-                            </v-list-item-avatar>
-                          </v-list-item>
-                        </v-card>
-                      </v-hover>
-                    </drag>
-                  </v-col>
-                </v-row>
-              </div>
-            </v-list>
-          </v-col>
-        </v-row>
+                              {{ item.UPDATE_TITLE }}
+                              <v-icon right>mdi-star</v-icon>
+                            </v-chip>
+                          </div>
+                          <!------------------>
+                          <v-hover v-slot:default="{ hover }">
+                            <v-card
+                              aria-selected="true"
+                              class="mx-auto"
+                              outlined
+                              style="cursor:pointer;"
+                              @click="selectSite(item, item.URL_KEY)"
+                              @dblclick="goSourceSite(item, $event)"
+                              :key="item.URL_KEY"
+                              ref="siteList"
+                            >
+                              <v-list-item three-line>
+                                <v-expand-transition>
+                                  <div
+                                    v-if="hover"
+                                    class="v-card--reveal white--text text-lg-right pr-3"
+                                    style="height: 30%;z-index: 99;"
+                                  >
+                                    <v-btn
+                                      @click="setFavorite($event, item)"
+                                      small
+                                      color="warning"
+                                    >
+                                      <v-icon>mdi-star</v-icon>
+                                    </v-btn>
+                                  </div>
+                                </v-expand-transition>
+
+                                <v-list-item-content>
+                                  <v-list-item-title
+                                    style="padding-bottom: 5px;"
+                                  >
+                                    {{ item.UPDATE_TITLE }}
+                                  </v-list-item-title>
+                                  <v-list-item-subtitle
+                                    style="font-size: 13px;"
+                                  >
+                                    <span
+                                      v-if="item.OG_DESCRIPTION !== 'undefined'"
+                                      >{{ item.OG_DESCRIPTION }}</span
+                                    >
+                                  </v-list-item-subtitle>
+                                  <v-list-item-subtitle
+                                    style="font-size: 10px; margin-top: 4px;"
+                                  >
+                                    <v-icon
+                                      size="16px"
+                                      color="green"
+                                      left
+                                      style="margin-right: 1px;"
+                                      v-if="
+                                        item.CATEGORY_NAME !== 'NO_CATEGORY'
+                                      "
+                                      >mdi-folder-outline
+                                    </v-icon>
+                                    {{ item.CATEGORY_NAME }}
+
+                                    <v-icon
+                                      size="16px"
+                                      color="yellow"
+                                      left
+                                      style="margin-right: 1px;"
+                                      v-if="item.FL_FAVORITE === 'Y'"
+                                      >mdi-star
+                                    </v-icon>
+                                    <v-icon
+                                      size="16px"
+                                      color="red"
+                                      left
+                                      style="margin-right: 1px;"
+                                      v-if="item.FL_READMODE === 'N'"
+                                      >mdi-shield-off-outline
+                                    </v-icon>
+                                  </v-list-item-subtitle>
+                                </v-list-item-content>
+
+                                <v-list-item-avatar
+                                  tile
+                                  size="70"
+                                  style="margin-top: 14px;padding-bottom: 0px;margin-bottom: 0px;"
+                                  color="grey"
+                                >
+                                  <v-img
+                                    v-if="item.OG_IMAGE !== 'undefined'"
+                                    :src="checkImagePath(item.OG_IMAGE)"
+                                  ></v-img>
+                                </v-list-item-avatar>
+                              </v-list-item>
+                            </v-card>
+                          </v-hover>
+                        </drag>
+                      </v-col>
+                    </v-row>
+                  </div>
+                </v-list>
+              </v-col>
+            </v-row>
+          </v-card-text>
+        </v-card>
       </v-col>
 
       <!-- 본문:프리뷰 -->
-      <v-col cols="9" :style="documentHeightStyle" v-if="currentSite !== ''">
+      <v-col
+        cols="9"
+        :style="documentHeightStyle"
+        v-if="currentSite !== ''"
+        class="pl-0 pt-0 pb-0"
+      >
         <v-row>
-          <v-col cols="12" class="ml-0 pl-0">
+          <v-col cols="12" class="ml-0">
             <PreviewPage
               :currentSite="currentSite"
               :youtubeVideoId="youtubeVideoId"
@@ -180,6 +205,7 @@ export default {
   data: () => ({
     documentHeightStyle: "max-height: 660px;",
     reviewAreaHeightStyle: "max-height: 660px;",
+    listPanelAreaHeightStyle: "max-height: 400px;",
     listAreaHeightStyle: "max-height: 400px;",
     sites: [],
     highlights: [],
@@ -278,9 +304,11 @@ export default {
       //"max-height: " + (document.documentElement.clientHeight - 84) + "px;";
 
       this.reviewAreaHeightStyle =
-        "height: " + (document.documentElement.clientHeight - 237) + "px;";
+        "height: " + (document.documentElement.clientHeight - 226) + "px;";
       this.listAreaHeightStyle =
-        "max-height: " + (document.documentElement.clientHeight - 126) + "px;";
+        "max-height: " + (document.documentElement.clientHeight - 190) + "px;";
+      this.listPanelAreaHeightStyle =
+        "height: " + (document.documentElement.clientHeight - 87) + "px;";
       this.documentHeightStyle =
         "max-height: " + (document.documentElement.clientHeight - 110) + "px;";
     },
